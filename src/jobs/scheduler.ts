4ee2fc2bf999bedb75345ba2344f2ce677b55836
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-04 15:46:00 +0100

    Change the format for scheduling jobs

--- src/jobs/scheduler.ts
+++ src/jobs/scheduler.ts
@@ -3,11 +3,28 @@ import Invoice, { InvoiceDocument } from "../models/invoice.model"
 
 import agenda from "./agendaInstance"
 
+const splitDate = (dateStr: string) => {
+    
+    const pattern = new RegExp("(\d{4})[-/]?(\d{2})[-/]?(\d{2})")
+    // const dates = dateStr.split(pattern)
+    const match = pattern.exec(dateStr)
+
+    if (match) {
+        return match.slice(1)
+    } else {
+        return null
+    }
+}
 
 const scheduler = {
-    dueDateMail: async (invoice: DocumentDefinition<InvoiceDocument>, date: Date) => {
-        // parse the date using date-fns
-        const dueDate = new Date(date)
+    dueDateMail: async (invoice: DocumentDefinition<InvoiceDocument>, date: string) => {
+        const splittedDate = splitDate(date)
+
+        if (!splittedDate) {
+            return
+        }
+
+        const dueDate = new Date(parseInt(splittedDate[0]), parseInt(splittedDate[1]) - 1, parseInt(splittedDate[2]))
         
         await agenda.schedule(dueDate, 'send-mail-on-due-date', { id: invoice._id })
     },
